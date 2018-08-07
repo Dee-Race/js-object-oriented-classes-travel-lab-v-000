c251ffb3ee94d7aa024fb85e7a4d11860b516410
Dee Race <deerace12@gmail.com>
2018-08-07 14:54:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,29 @@ class Driver {
 }
 
 class Route {
-  constructor(horizontal, vertical) {
-    this.horizontal = horizontal
-    this.vertical = vertical
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  avenueToInt(ave) {
+    return eastWest.indexOf(ave)
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+    let horizontalDist = this.avenueToInt(this.endingLocation.horizontal) - this.avenueToInt(this.beginningLocation.horizontal)
+    let verticalDist = this.endingLocation.vertical - this.beginningLocation.vertical
+    return (Math.abs(horizontalDist) + Math.abs(verticalDist))
+  }
+
+  estimatedTime(peak) {
+    if(peak) {
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
   }
   
 }
